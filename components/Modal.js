84a61e8bda0f161e6fb5b297d04d16c287c3a408
letmaticode <sdevmatt@gmail.com>
2022-10-03 10:16:57 -0300

    fix responsive design + modal

--- components/Modal.js
+++ components/Modal.js
@@ -1,16 +1,14 @@
-export default function Modal({showModal, modal}) {
+export default function Modal({ showModal, modal }) {
 
     return (
-        <aside className={showModal ? 'modal' : 'modal--hidden' }>
-            <article className="modal__header">
-                <p className="modal__header-title">
-                    RULES
-                </p>
+        <aside className={showModal ? 'modal' : 'modal--hidden'}>
+            <p className="modal__header-title">
+                RULES
+            </p>
 
-                <button onClick={modal} className="modal__header-button">
-                    X
-                </button>
-            </article>
+            <button onClick={modal} className="modal__header-button">
+                X
+            </button>
 
             <img className="modal__img" src="/images/image-rules.svg" />
         </aside>
